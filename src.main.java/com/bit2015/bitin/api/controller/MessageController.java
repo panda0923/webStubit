e865b8d963d12193aee817f99dede5b86a5acb09
bnmjkl26 <bnmjkl26@naver.com>
2015-12-07 19:47:45 +0900

    hyunjun commit2

---
+++ src.main.java/com/bit2015/bitin/api/controller/MessageController.java
@@ -0,0 +1,42 @@
+package com.bit2015.bitin.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bit2015.bitin.service.MessageService;
+import com.bit2015.bitin.service.UserService;
+
+@Controller
+public class MessageController {
+
+	@Autowired
+	MessageService userService;
+	
+
+	@ResponseBody
+	@RequestMapping("/messagelist")
+	public Map<String, Object> messageList(
+			@RequestParam(value = "userId", required=false)String userId	) {
+		
+		HashMap<String, Object> retMap = new HashMap<String, Object>();
+		List<String> retList = MessageService.getMessageList(userId);
+		
+		retMap.put("result", "success");
+		retMap.put("data", retList);
+		
+		
+		/*if( 문제){
+			retMap.put("result", "fail");
+			retMap.put("message", "실패이유");
+		}*/
+		
+		return retMap;
+	}
+}
